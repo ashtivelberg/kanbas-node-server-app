787400850b7217e0b7c21c878c3a4b492951a95b
ashtivelberg <arielshtivelberg@gmail.com>
2024-04-17 01:25:41 -0400

    single session

--- App.js
+++ App.js
@@ -13,29 +13,29 @@ const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 
-app.use(
-    cors({
-      credentials: true,
-      origin: process.env.FRONTEND_URL
-    })
-   );
+// app.use(
+//     cors({
+//       credentials: true,
+//       origin: process.env.FRONTEND_URL
+//     })
+//    );
 
-  const sessionOptions = {
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  };
+//   const sessionOptions = {
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//   };
 
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-      domain: process.env.HTTP_SERVER_DOMAIN,
-    };
-  }
-  app.use(session(sessionOptions));
-  
+//   if (process.env.NODE_ENV !== "development") {
+//     sessionOptions.proxy = true;
+//     sessionOptions.cookie = {
+//       sameSite: "none",
+//       secure: true,
+//       domain: process.env.HTTP_SERVER_DOMAIN,
+//     };
+//   }
+//   app.use(session(sessionOptions));
+app.use(cors());
 app.use(express.json());
 UserRoutes(app);
 ModuleRoutes(app);
